2a2b548d17af47131a760cd1114a0e5facc93f51
Baran Yağış Kızmaz <baranyagiskizmaz2@gmail.com>
2020-02-17 21:27:14 +0300

    Siparis-detaylar

---
+++ KafeKod.Data/SiparisDetay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafeKod.Data
+{
+   public  class SiparisDetay
+    {
+        public int Adet { get; set; }
+
+        public string UrunAd { get; set; }
+
+        public decimal BirimFiyat { get; set; }
+
+        public decimal Tutar() => Adet * BirimFiyat;
+
+        public void SarkiSoyle() => Console.WriteLine("Havada ay ışığı");
+
+    }
+}
